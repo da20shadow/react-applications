7604c3f0daf7f94ec9396d6e525440bca946c31f
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-20 22:56:18 +0200

    Added CORS middlware

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import {dbInit} from "./config/index.js";
 import routes from './routes.js';
 import {ENV} from "./constants/index.js";
+import {CORS} from "./middlewares/index.js";
 
 const app = express();
 
@@ -12,8 +13,12 @@ app.use(express.urlencoded({extended: false}));
 //Add cookie parser
 app.use(cookieParser());
 
+//Set CORS
+app.use(CORS);
+
 //Add routes
 app.use(routes);
+
 
 //Connect with db and run the server!
 dbInit().then(() => {
---
+++ back-end/middlewares/corsMiddleware.js
@@ -0,0 +1,8 @@
+const corsMiddleware = async (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+}
+
+export default corsMiddleware;
