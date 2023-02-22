6e619a26cd84f97965522ed50897a5f423f97538
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-22 12:17:01 +0200

    updated the origins

--- back-end/middlewares/corsMiddleware.js
+++ back-end/middlewares/corsMiddleware.js
@@ -1,5 +1,5 @@
 const corsMiddleware = async (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000,http://192.168.0.102:3000');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // enable sending credentials
