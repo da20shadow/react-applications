e20ab409784250841f3b765b65384d20476a02d9
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-22 07:42:24 +0200

    create custom hook

---
+++ to-do-app/src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+const useLocalStorage = (key, initialValue) => {
+    const [state, setState] = useState(() => {
+        try {
+            let user = localStorage.getItem(key);
+            return user ? JSON.parse(user) : initialValue;
+        } catch (err) {
+            return initialValue;
+        }
+    });
+
+    const setItem = (value) => {
+        try {
+            localStorage.setItem(key, JSON.stringify(value));
+            setState(value);
+        }catch (err){
+            console.log('Error: ',err);
+        }
+    }
+    return [state, setItem];
+}
+
+export default useLocalStorage;
